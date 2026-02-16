fadf0debb852a65288d9101eb67b1b819b364ab7
remakefactory <215389873+remakefactory@users.noreply.github.com>
2026-02-17 06:36:15 +0800

    lang

--- src/main/java/com/ref/aea/data/LangProvider.java
+++ src/main/java/com/ref/aea/data/LangProvider.java
@@ -4,7 +4,17 @@ import appeng.core.localization.LocalizationEnum;
 import com.ref.aea.AEA;
 import com.ref.aea.core.localization.AEAButtonToolTips;
 import com.ref.aea.core.localization.AEAGuiText;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.LanguageProvider;
 
 public class LangProvider extends LanguageProvider {
@@ -12,8 +22,24 @@ public class LangProvider extends LanguageProvider {
     super(packOutput, AEA.MOD_ID, "en_us");
   }
 
+  public static Map<Supplier<? extends Block>, String> Blocks = new ConcurrentHashMap<>();
+  public static Map<Supplier<? extends Item>, String> Items = new ConcurrentHashMap<>();
+  public static Map<Supplier<ItemStack>, String> ItemStacks = new ConcurrentHashMap<>();
+  public static Map<Supplier<? extends Enchantment>, String> Enchantments =
+      new ConcurrentHashMap<>();
+  public static Map<Supplier<? extends MobEffect>, String> MobEffects = new ConcurrentHashMap<>();
+  public static Map<Supplier<? extends EntityType<?>>, String> EntityTypes =
+      new ConcurrentHashMap<>();
+
   @Override
   protected void addTranslations() {
+    Blocks.forEach(this::addBlock);
+    Items.forEach(this::addItem);
+    ItemStacks.forEach(this::addItemStack);
+    Enchantments.forEach(this::addEnchantment);
+    MobEffects.forEach(this::addEffect);
+    EntityTypes.forEach(this::addEntityType);
+
     this.addEnum(AEAGuiText.class);
     this.addEnum(AEAButtonToolTips.class);
     this.add("tooltip.mirror.sourcePos.info", "[%s,%s,%s] (%s) %s");
